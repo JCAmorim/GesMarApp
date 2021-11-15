7496690f074e32a0d46981c72fb43e1a1f2ca277
João Amorim <Joaoamorim2@gmail.com>
2021-11-15 16:41:57 +0000

    Commit de Inicio

--- src/pt/gesmarapp/GesMarApp.java
+++ src/pt/gesmarapp/GesMarApp.java
@@ -8,7 +8,7 @@ public class GesMarApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Bem vindo ao gestor de marcações!!!!");
+        System.out.println("Bem vindo ao gestor de marcações de consultas e ou vacinação!!!!");
     }
     
 }
