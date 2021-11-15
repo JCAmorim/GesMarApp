feac47d4000be4819dfeeb1d94c2789362157ce4
João Amorim <Joaoamorim2@gmail.com>
2021-11-15 16:39:05 +0000

    Commit de Inicio

---
+++ src/pt/gesmarapp/GesMarApp.java
@@ -0,0 +1,14 @@
+
+package pt.gesmarapp;
+
+
+public class GesMarApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao gestor de marcações!!!!");
+    }
+    
+}
---
+++ src/pt/gesmarapp/model/Marcação.java
@@ -0,0 +1,11 @@
+package pt.gesmarapp.model;
+
+
+public class Marcação {
+//    idTipo
+//    Tipo consulta
+//    data    
+//    Hora
+//    
+//    
+}
