6c28df5e44c8594da0724ab4d6ceb95133a817cd
João Amorim <Joaoamorim2@gmail.com>
2021-11-15 16:56:48 +0000

    Classe utilizador

---
+++ src/pt/gesmarapp/model/Utilizador.java
@@ -0,0 +1,14 @@
+
+package pt.gesmarapp.model;
+
+
+public class Utilizador {
+    
+//    nomeCompleto   //CC
+//    númeroCC     //Cartão do Cidadão   
+//    dataNascimento //data de Nascimento
+//    morada  //morada        
+//           
+//    
+    
+}
